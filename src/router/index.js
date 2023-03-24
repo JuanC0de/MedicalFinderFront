8a4ad47daaaeceed7f0f57631225b1acd8758503
Vanessa Silva <karelyssilva08122003@gmail.com>
2023-03-23 21:28:56 -0500

    Ejemplos de componentes en vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -14,8 +14,20 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/SegundoHome',
+        name: 'Home2',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home/Home.vue'),
+      }
     ],
   },
+  {
+    path: '/home2',
+    component: () => import('@/views/Home/Home.vue')
+  }
 ]
 
 const router = createRouter({
