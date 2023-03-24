25333630812f5504a25515d33647bf8d7f5e78fa
Maria Jose Paz Garcia <majopazgarcia25@gmail.com>
2023-03-24 16:35:37 -0500

    first part of logging in

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,11 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home/Home.vue'),
+      },
+      {
+        path: '/IniciarSesion',
+        name: 'Iniciar Sesion',
+        component: () => import('@/views/StartSection/StartSection.vue'),
       }
     ],
   },
---
+++ src/views/StartSection/StartSection.vue
@@ -0,0 +1,22 @@
+<script setup>
+import FormStartSection from '../../layouts/FormStartSection/FormStartSection.vue'
+</script>
+
+<template>
+    <div class="my-view">
+        <FormStartSection class="formContainer"/>
+    </div>   
+</template>
+
+<style>
+    .my-view {
+        background-image: url('@/assets/images/pacienteMedico.png');
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        margin: 1px;
+    }
+    /* .formContainer{
+        margin: 5cm 8cm 2cm 4cm;
+    } */
+</style>
