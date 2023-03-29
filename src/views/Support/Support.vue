dc928a320732ed1e9d8e87be35bb9fc030a1b84c
Vanessa Silva <Karelyssilva08122003@gmail.com>
2023-03-29 17:05:48 -0500

    modulo soporte Vanessa

--- src/views/Support/Support.vue
+++ src/views/Support/Support.vue
@@ -1,10 +1,34 @@
-<script>
-import FormSupport from "@/layouts/FormSupport/FormSupport.vue"
+<script setup>
+import FormSupport  from '@/layouts/FormSupport/FormSupport.vue'
+import SupportingParagraph from '@/layouts/SupportingParagraph/SupportingParagraph.vue'
 
 </script>
 
 <template>
-    <div class="ViewSupport">
-        <FormSupport/>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+            <div class="paragraphSupp">
+                <SupportingParagraph/>
+            </div>
+        </v-col>
+        <v-col cols="6">
+            <FormSupport/>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-card>
+        <v-img
+            src="@/assets/images/imgSupport/imgSupport.png"
+            height="400"            
+        ></v-img>
+    </v-card>
 </template>
+
+<style>
+    .paragraphSupp{
+        margin-top: 20%;
+        margin-left: 30px;
+    }
+</style>
