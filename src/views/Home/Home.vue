d2074f8b883a80d265239b4b7ab7fbc20c6367b7
Vanessa Silva <karelyssilva08122003@gmail.com>
2023-05-04 21:09:08 -0500

    Solución deL ERROR DEL MIN-WIDTH MALDITO

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,19 +6,19 @@
     </v-card> -->
 
     <!-- Buscador -->
-    <v-container class="mb-8">
-      <v-row no-gutters>
+    <v-container class="mb-8 w-100" width="100%">
+      <v-row >
       <v-col cols="6">
         <div class="text-h4 text-white mb-10">¡Encuentra el especialista que necesitas!</div>
 
-          <v-row no-gutters>
+          <v-row >
             <v-card class="w-100 elevation-10">
               <template v-slot:title>
                 Selecciona la ciudad y la especialidad
               </template>
 
               <v-card-text>
-                <v-row no-gutters justify="center" align="center">
+                <v-row  justify="center" align="center">
                   <v-col cols="5">
                     <v-select class="ma-1" label="Select"
                       :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
@@ -35,7 +35,7 @@
                     </v-btn>
                   </v-col>
                 </v-row>
-                <!-- <v-row no-gutters justify="end">
+                <!-- <v-row  justify="end">
                   <v-col  cols="2">
                     <v-btn class="ma-2 w-100" variant="outlined">
                         Buscar
@@ -57,14 +57,14 @@
 
     <v-card class="bg-white elevation-10">
       <v-container class="mb-8">
-        <v-row no-gutters class="mb-8">
+        <v-row  class="mb-8">
           <v-col cols="12 justify-end align-center">
             <div class="text-h4 text-MediumCyan" justify="center" align="center">
               Consejos para ingresar
             </div>
           </v-col>
         </v-row>
-        <v-row no-gutters>
+        <v-row >
           <v-col cols="4">
             <v-card class="mx-auto" max-width="450">
               <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
@@ -165,14 +165,14 @@
       </div>
       <div class="bg-PrincipalCyan" height="800px">
         <v-container class="mb-8">
-          <v-row no-gutters class="mb-8">
+          <v-row  class="mb-8">
             <v-col cols="12 justify-end align-center">
               <div class="text-h4 text-MediumCyan" justify="center" align="center">
                 Consejos para ingresar
               </div>
             </v-col>
           </v-row>
-          <v-row no-gutters>
+          <v-row >
             <v-col cols="4">
               <v-card width="400" title="This is a title" subtitle="This is a subtitle" text="This is content"></v-card>
             </v-col>
@@ -296,94 +296,11 @@ export default {
   background-color: transparent;
 }
 
-/* .menu {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  background-color: #f2f2f2;
-  background-color: rgba(0, 0, 0, 0);
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 999;
-  
+@media only screen and (min-width: 1280px) {
+  .v-container {
+    max-width: 1800px !important;
 }
-
-.menu-logo img {
-  height: 50px;
 }
-
-.menu-options {
-  list-style: none;
-  display: flex;
-  margin: 0;
-  padding: 0;
-}
-
-.menu-options li {
-  margin-right: 10px;
-}
-
-.menu-options li a {
-  color: #333;
-  text-decoration: none;
-  font-size: 16px;
-  padding: 5px 10px;
-  border-radius: 5px;
-  transition: all 0.2s ease-in-out ;
-}
-
-.menu-options li a:hover {
-  background-color: #1C565C;
-  color:#f2f2f2;
-}
-
-.menu-buttons {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-.menu-buttons .btn {
-  margin-right: 10px;
-  color: #fff;
-  background-color: #333;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  text-decoration: none;
-  transition: all 0.2s ease-in-out;
-}
-
-.menu-buttons .btn:hover {
-  background-color: #666;
-}
-
-.menu-buttons .btn-primary {
-  background-color: #007bff;
-}
-
-/* Estilos para pantallas pequeñas 
-@media (max-width: 768px) {
-  .menu {
-    flex-direction: column;
-  }
-  
-  .menu-options {
-    margin-top: 10px;
-  }
-  
-  .menu-options li {
-    margin-right: 0;
-    margin-bottom: 10px;
-  }
-  
-  .menu-buttons {
-    margin-top: 10px;
-  }
-} */
 </style>
 
 
