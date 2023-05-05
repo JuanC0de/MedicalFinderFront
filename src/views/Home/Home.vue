5c60bb75c119ad2a6b56ba4215e56db7df477842
Jose Ordoñez <joseluisordonezc@gmail.com>
2023-05-04 19:28:05 -0500

    Solución de errores

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar/>
+  <Navbar />
   <div class="container justify-center">
     <div class="h-auto d-flex justify-center text-white backTransparent">
       <v-toolbar class="d-flex flex-row-reverse mb-6 bg-surface-variant w-75 rounded-b-lg elevation-20">
@@ -17,54 +17,52 @@
           Button
         </v-btn>
         <v-btn>
-          {{boton1}}
+          {{ boton1 }}
         </v-btn>
         <v-btn>
           Button
         </v-btn>
-        
+
       </v-toolbar>
     </div>
     <!--  -->
-    <v-container class="mb-8" >
-      <v-row >
-      <v-col cols="6">
-        <div class="text-h4 text-white mb-10">¡Encuentra el especialista que necesitas!</div>
+    <v-container class="mb-8">
+      <v-row>
+        <v-col cols="6">
+          <div class="text-h4 text-white mb-10">¡Encuentra el especialista que necesitas!</div>
 
-        <v-row >
-            <v-card
-              class="w-100 elevation-10">
-          <v-row no-gutters>
+          <v-row>
             <v-card class="w-100 elevation-10">
-              <template v-slot:title>
-                Selecciona la ciudad y la especialidad
-              </template>
 
-              <v-card-text>
-                <v-row justify="center" align="center">
-                  <v-col  cols="5">
-                    <v-select class="ma-1" label="Select" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
-                    </v-select>
-                  </v-col>
-                  <v-col cols="5">
-                    <v-select class="ma-1" label="Select"
-                      :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
-                    </v-select>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-btn class="ma-1 w-100" variant="outlined">
-                      Buscar
-                    </v-btn>
-                  </v-col>
+              <v-row no-gutters>
+                <v-card class="w-100 elevation-10">
+                  <template v-slot:title>
+                    Selecciona la ciudad y la especialidad
+                  </template>
+
+                  <v-card-text>
+                    <v-row justify="center" align="center">
+                      <v-col cols="5">
+                        <v-select class="ma-1" label="Select"
+                          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
+                        </v-select>
+                      </v-col>
+                      <v-col cols="5">
+                        <v-select class="ma-1" label="Select"
+                          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']">
+                        </v-select>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-btn class="ma-1 w-100" variant="outlined">
+                          Buscar
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-card>
+              </v-row>
+                </v-card>
                 </v-row>
-              </v-card-text>
-            </v-card>
-          </v-row>
-
-        </v-col>
-
-        <v-col>
-
         </v-col>
       </v-row>
     </v-container>
@@ -165,44 +163,35 @@
             </v-card>
           </v-col>
         </v-row>
-    </v-container>
-    <div class="transparent" height="800px">
-      <h1>------</h1>
-    </div>
-     
- <!-- Footer -->
-    <div class="" height="800px">
-  <v-footer class="bg-grey-lighten-1">
-    <v-row justify="center" >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Medical Finder</strong> 
-      </v-col>
-      <v-col class="text-center mt-4" cols="12">
-        <a href="#">Privacy Policy</a>
-        <span class="mx-2">•</span>
-        <a href="#">Terms of Service</a>
-      </v-col>
-      <v-col class="text-center mt-4" cols="12">
-        <span>&copy; {{ new Date().getFullYear() }} Medical Finder</span>
-      </v-col>
-    </v-row>
-  </v-footer>
-</div>
-<!-- Footer fin -->
-  </v-card>
-  </div>
+      </v-container>
+      <div class="transparent" height="800px">
+        <h1>------</h1>
+      </div>
 
-  
+      <!-- Footer -->
+      <div class="" height="800px">
+        <v-footer class="bg-grey-lighten-1">
+          <v-row justify="center">
+            <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
+              {{ link }}
+            </v-btn>
+            <v-col class="text-center mt-4" cols="12">
+              {{ new Date().getFullYear() }} — <strong>Medical Finder</strong>
+            </v-col>
+            <v-col class="text-center mt-4" cols="12">
+              <a href="#">Privacy Policy</a>
+              <span class="mx-2">•</span>
+              <a href="#">Terms of Service</a>
+            </v-col>
+            <v-col class="text-center mt-4" cols="12">
+              <span>&copy; {{ new Date().getFullYear() }} Medical Finder</span>
+            </v-col>
+          </v-row>
+        </v-footer>
+      </div>
+      <!-- Footer fin -->
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -210,7 +199,7 @@ import Navbar from '@/components/Navbar.vue';
 
 export default {
   name: 'HomeView',
-  components:{
+  components: {
     Navbar
   },
   props: {
@@ -238,7 +227,6 @@ export default {
 .backTransparent {
   background-color: transparent;
 }
-
 </style>
 
 
