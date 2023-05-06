9484cf5e1d3fb1444cf87f4451cc92a6a4ce7e83
MajoPaz <majopazgarcia25@gmail.com>
2023-05-06 13:12:17 -0500

    tamaño de la imagen con media en mision y vision

--- src/layouts/MissionComponent/MissionComponent.vue
+++ src/layouts/MissionComponent/MissionComponent.vue
@@ -17,16 +17,19 @@ export default {
   <v-container  align = "center">
     <v-row align = "center" justify="space-around">
       <v-col cols="6" order="1">
-        <v-sheet class="pa-1 ma-1">
-          <h2 
-        class="text-lg font-custom"
-        >{{ title }}</h2>
-          <p>{{ text }}</p>
+        <v-sheet class="pa-10 ma-1">
+          <h2 class="title-lg">
+            {{ title }}
+          </h2>
+          <p class="text-lg">
+            {{ text }}
+          </p>
         </v-sheet>
       </v-col>
       <v-col  cols="6" order="2">
         <v-sheet class="pa-2 ma-2">
           <v-img
+            class="img"
             src="@/assets/images/imgMissionVision/Mision1.png"
             :alt="title"
             width="100%"
@@ -36,3 +39,15 @@ export default {
     </v-row>
   </v-container>   
 </template>
+
+<style>
+  @media only screen and (min-width: 1383px) {
+    .img {
+      width: 600px;
+      height: 550px;
+    }
+  }
+  .text-lg {
+    font-size: 20px; /* Cambiar el tamaño de letra */
+  }
+</style>
