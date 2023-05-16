5392ab03d00b6a4aa22213db0b4d2379af891269
Maria Jose Paz Garcia <majopazgarcia25@gmail.com>
2023-05-16 16:19:06 -0500

    arreglando acerca de nosotros, card con img

--- src/views/AboutUs/AboutUs.vue
+++ src/views/AboutUs/AboutUs.vue
@@ -2,16 +2,16 @@
 import MissionComponent from '@/layouts/MissionComponent/MissionComponent.vue';
 import VisionComponent from '@/layouts/VisionComponent/VisionComponent.vue';
 // import CircularImageCard from '@/layouts/CircularImageCard/CircularImageCard.vue';
-import CardAboutUs from '@/layouts/CardAboutUs/CardAboutUs.vue'
+// import CardAboutUs from '@/layouts/CardAboutUs/CardAboutUs.vue'
 import Navbar from '@/components/Navbar.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
+
 
 export default {
   name: 'Aboutus',
   components: {
     MissionComponent,
     VisionComponent,
-    CardAboutUs,
     Navbar,
     FooterComponent
   },
@@ -28,6 +28,7 @@ export default {
 }
 </script>
 
+
 <template>
   <Navbar/>
     <MissionComponent
@@ -38,147 +39,204 @@ export default {
         title="Visión"
         text="Pretendemos ayudar y Satisfacer de manera eficaz y eficiente las necesidades del cuidado, mejorando cada vez más en diferentes aspectos como la atención que tenga el ciudadano y el ambiente dentro de nuestro sistema, también queremos llegar inicialmente a todo el territorio de nuestro país, y así, esparcirnos a nivel mundial."
     />
-    <!-- <v-container align = "center">
+    <v-container align = "center"> 
       <v-row>
         <v-col order="1">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageJuanAndres"
-              title="Juan Andrés Bolaño Ospina"
-              subtitle="Scrum Master"
-              textCard="Facilita el proceso de Scrum y elimina obstáculos para el equipo."
-            />
+            <v-card class="card">
+              <v-avatar size=80 class="align-center">
+                <img
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/JuanAndres.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Juan Andrés Bolaño Ospina
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Scrum Master</p>
+                    <p class="mt-2">Facilita el proceso de Scrum y elimina obstáculos para el equipo.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card>
           </v-sheet>
         </v-col>
         <v-col order="6">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageKarelysVanessa"
-              title="Karelys Vanessa Silva Quitian"
-              subtitle="Product Owner"
-              textCard="Define los requisitos del producto y prioriza el trabajo del equipo."
-            />
+            <v-card class="card">
+              <v-avatar size=80 class="align-center">
+                <img
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/Vanessa.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Karelys Vanessa Silva Quitian
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Product Owner</p>
+                    <p class="mt-2">Define los requisitos del producto y prioriza el trabajo del equipo.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card>
           </v-sheet>
         </v-col>
         <v-col order="12">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageJuanJose" 
-              title="Juan Jose Rojas Guaneme"
-              subtitle="Equipo de Desarrollo"
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."
-            />
+            <v-card>
+              <v-avatar>
+                <img
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/JuanJose.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Juan Jose Rojas Guaneme
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Equipo de Desarrollo</p>
+                    <p class="mt-2">Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card> 
           </v-sheet>
         </v-col>
       </v-row>
     </v-container>
-    <v-container align = "center">
+
+    <v-container align = "center"> 
       <v-row>
         <v-col order="1">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageLuisaFernanda"    
-              title="Luisa Fernanda Castiblanco"
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."           
-            />
+            <v-card class="card">
+              <v-avatar size=80 class="align-center">
+                <img  
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/Luisa.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Luisa Fernanda Castiblanco
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Equipo de Desarrollo</p>
+                    <p class="mt-2">Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card> 
           </v-sheet>
         </v-col>
         <v-col order="6">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageJoseLuis"  
-              title="Jose Luis Ordoñez Calderón "
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."           
-            />
+            <v-card class="card">
+              <v-avatar size=80 class="align-center">
+                <img
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/JoseLuis.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Jose Luis Ordoñez Calderón
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Equipo de Desarrollo</P>
+                    <p class="mt-2">Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card>
           </v-sheet>
         </v-col>
         <v-col order="12">
           <v-sheet class="pa-2 ma-2">
-            <CircularImageCard
-              :img="imageMariaJose" 
-              title="Maria Jose Paz García"
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."                  
-            />
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container> -->
-  
-    <v-container align = "center">
-      <v-row>
-        <v-col order="1">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :image-url="imageJuanAndres"
-              title="Juan Andrés Bolaño Ospina"
-              subtitle="Scrum Master"
-              textCard="Facilita el proceso de Scrum y elimina obstáculos para el equipo."
-            />
-          </v-sheet>
-        </v-col>
-        <v-col order="6">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :imgCard="imageKarelysVanessa"
-              title="Karelys Vanessa Silva Quitian"
-              subtitle="Product Owner"
-              textCard="Define los requisitos del producto y prioriza el trabajo del equipo."
-            />
-          </v-sheet>
-        </v-col>
-        <v-col order="12">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :imgCard="imageJuanJose"
-              title="Juan Jose Rojas Guaneme"
-              subtitle="Equipo de Desarrollo"
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."
-            />
+            <v-card>
+              <v-avatar>
+                <img
+                  class="imgIconoCard"
+                  src="@/assets/images/imgWorkingGroup/Majo.jpg"
+                  alt="icono-grupo"
+                />
+              </v-avatar>
+              <v-container>
+                  <v-card-title>
+                    Maria Jose Paz García
+                  </v-card-title>
+                  <v-card-text class="mt-5">
+                    <p>Equipo de Desarrollo</p>
+                    <p class="mt-2">Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum.</p>
+                  </v-card-text>
+              </v-container>
+            </v-card> 
           </v-sheet>
         </v-col>
       </v-row>
     </v-container>
-    <v-container align = "center">
-      <v-row>
-        <v-col order="1">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :imgCard="imageLuisaFernanda"
-              title="Luisa Fernanda Castiblanco"
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."
-            />
-          </v-sheet>
-        </v-col>
-        <v-col order="6">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :imgCard="imageJoseLuis"
-              title="Jose Luis Ordoñez Calderón "
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum."
-            />
-          </v-sheet>
-        </v-col>
-        <v-col order="12">
-          <v-sheet class="pa-2 ma-2">
-            <CardAboutUs
-              :imgCard="imageMariaJose"
-              title="Maria Jose Paz García"
-              subtitle="Equipo de Desarrollo" 
-              textCard="Construye el producto de acuerdo con los requisitos del Product Owner y el proceso de Scrum." 
-            />
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>    
   <FooterComponent/>  
 </template>
 
 <style>
+.imgIconoCard{
+  background-size: cover;
+  background-position: relative;
+  width: 100px;
+}
+.card {
+  position: relative;
+  width: 320px;
+  height: 320px;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 15px;
+  font-weight: bold;
+  border-radius: 15px;
+  cursor: pointer;
+}
+.card::before,
+.card::after {
+  position: absolute;
+  content: "";
+  width: 20%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  background-color: #3AB4C2;
+  transition: all 0.5s;
+}
+.card::before {
+  top: 0;
+  right: 0;
+  border-radius: 0 5px 0 100%;
+}
+.card::after {
+  bottom: 0;
+  left: 0;
+  border-radius: 0 100%  0 5px;
+}
+.card:hover::before,
+.card:hover:after {
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  transition: all 0.5s;
+}
+.card:hover:after {
+  content: "Medical Finder";
+}
+
 @media only screen and (min-width: 1280px) {
   .v-container {
     max-width: 1800px !important;
