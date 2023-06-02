15a40ff6370c18e3486b1dc491315a4ac9b957ef
Majo <majopazgarcia25@gmail.com>
2023-06-01 21:21:48 -0500

    comenzando el agendamiento de citas

---
+++ src/views/Find/ScheduleAppointments/ScheduleAppointments.vue
@@ -0,0 +1,12 @@
+<script>
+import AppointmentSchedulingForm from '@/layouts/AppointmentSchedulingForm/AppointmentSchedulingForm.vue'
+</script>
+
+<template>
+
+    <AppointmentSchedulingForm/>
+
+</template>
+
+<style>
+</style>
