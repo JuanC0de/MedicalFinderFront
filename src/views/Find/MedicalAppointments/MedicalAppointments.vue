63ffed46827848a6d40defb2f71d7f69e326dda4
Majo <majopazgarcia25@gmail.com>
2023-06-03 15:37:11 -0500

    formulario de agendar citas

--- src/views/Find/MedicalAppointments/MedicalAppointments.vue
+++ src/views/Find/MedicalAppointments/MedicalAppointments.vue
@@ -1,12 +1,21 @@
 <script setup>
+import Navbar from '@/components/Navbar.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 import FormMedicalAppointments from '@/layouts/FormMedicalAppointments/FormMedicalAppointments.vue';
 import ProfileMedicalAppointments from '@/layouts/ProfileMedicalAppointments/ProfileMedicalAppointments.vue';
 </script>
 
 <template>
-    <FormMedicalAppointments/>
-    <ProfileMedicalAppointments/>
+    <Navbar/>
+    <v-container>
+        <v-row align="center">
+            <v-col cols="12" md="6">
+                <FormMedicalAppointments />
+            </v-col>
+            <v-col cols="12" md="6">
+                <ProfileMedicalAppointments />
+            </v-col>
+        </v-row>
+    </v-container>
+    <FooterComponent/>
 </template>
-
-<style>
-</style>
