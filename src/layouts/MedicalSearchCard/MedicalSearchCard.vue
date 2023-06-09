c93b9bcf798cd09edc64f1ea3549b93d7919bef5
Juan Andrés Bolaño <juabo008@gmail.com>
2023-06-08 20:46:18 -0500

    Card del buscador medico en proceso

--- src/layouts/MedicalSearchCard/MedicalSearchCard.vue
+++ src/layouts/MedicalSearchCard/MedicalSearchCard.vue
@@ -1,7 +1,107 @@
 <script>
 </script>
 <template>
+  <div class="cardMed">
+    <div class="head">
+      <div class="circle"></div>
+      <div class="img">
+        <img
+          src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1049&q=80"
+          alt=""
+        />
+      </div>
+    </div>
 
+    <div class="description">
+      <h3>Nombre Medico</h3>
+      <h4>Especialidad</h4>
+      <p>Edad: {{ edad }}</p>
+      <p>ciudad: {{ ciudad }}</p>
+    </div>
+
+    <div class="contact">
+      <a href="#">Contact</a>
+    </div>
+  </div>
 </template>
 <style>
+.cardMed {
+  position: relative;
+  width: 800px;
+  height: 400px;
+  background: LightCyan;
+  border-radius: 5px;
+  overflow: hidden;
+  transition: 0.5s;
+  margin: 15px 20px;
+}
+.cardMed:hover {
+  box-shadow: 0 5px 15px rgba(3, 89, 92, 0.5);
+  transform: translateY(-15px);
+}
+.cardMed .head {
+  height: 125px;
+  width: 100%;
+  position: relative;
+}
+.cardMed .head .circle {
+  position: absolute;
+  left: 18%;
+  width: 500px;
+  height: 300px;
+  border-radius: 50%;
+  background: #42ccdb;
+  bottom: 0;
+}
+.cardMed .head .img {
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  background: #fff;
+  padding: 5px;
+  border-radius: 50%;
+  bottom: -30%;
+  left: 50%;
+  transform: translate(-50%);
+}
+.cardMed .head .img img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.cardMed .description {
+  height: 200px;
+  padding: 40px 20px 20px 20px;
+  border-bottom: solid 1px rgba(6, 74, 76, 0.18);
+  text-align: center;
+}
+.cardMed .description h3 {
+  color: #3ab4c2;
+}
+.cardMed .description h4 {
+  color: #42ccdb;
+}
+.cardMed .description p {
+  margin-top: 20px;
+  font-size: 15px;
+}
+.cardMed .contact {
+  width: 100%;
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.cardMed .contact a {
+  text-decoration: none;
+  color: #fff;
+  background: #42ccdb;
+  padding: 5px 20px;
+  border-radius: 5px;
+  transition: 0.3s;
+}
+.cardMed .contact a:hover {
+  background: #2f919c;
+}
 </style>
--- src/views/Find/MedicalFinder.vue
+++ src/views/Find/MedicalFinder.vue
@@ -1,193 +1,185 @@
+<script setup>
+import MedicalSearchCard from '@/layouts/MedicalSearchCard/MedicalSearchCard.vue'
+
+</script>
+
 <template>
   <Navbar />
   <div class="justify-center">
-
     <!-- Buscador -->
     <div height="300px">
-      <v-container class=" w-100" width="100%" >
+      <v-container class="w-100" width="100%">
         <v-row justify="center">
           <v-col cols="12">
             <v-row>
               <v-col cols="12" align="center">
-              <p class="buscadorTittle text-PrincipalCyan mt-3 mb-3">Buscador de especialistas</p>
+                <p class="buscadorTittle text-PrincipalCyan mt-3 mb-3">
+                  Buscador de especialistas
+                </p>
               </v-col>
             </v-row>
             <v-row>
-                <template v-slot:title>
-                  Selecciona la ciudad y la especialidad
-                </template>
-                <v-row justify="center" align="center">
-                    <v-col cols="5">
-                      <!-- <AccordionCities class="ma-1" /> -->
-                      <v-col>
-                        <v-autocomplete
-                          v-model="city"
-                          :items="this.ciudades"
-                          label="Ciudades"
-                          persistent-hint
-                        >
-                          <template v-slot:append-outer>
-                            <v-slide-x-reverse-transition
-                              mode="out-in"
-                            >
-                            </v-slide-x-reverse-transition>
-                          </template>
-                        </v-autocomplete>
-                      </v-col>
-                    </v-col>
-                    <v-col cols="5">
-                      <!-- <SelectSpecialists class="ma-1"/> -->
-                      <v-col>
-                        <v-autocomplete
-                          v-model="especialidad"
-                          :items="this.especialidades"
-                          label="Especialidades"
-                          persistent-hint
-                        >
-                          <template v-slot:append-outer>
-                            <v-slide-x-reverse-transition
-                              mode="out-in"
-                            >
-                            </v-slide-x-reverse-transition>
-                          </template>
-                        </v-autocomplete>
-                      </v-col>
-                    </v-col>
-                    <v-col cols="2">
-                      <v-btn class="text-White ma-1 w-100 bg-PrincipalCyan" variant="text">
-                        Buscar
-                      </v-btn>
-                    </v-col>
-                </v-row>
+              <template v-slot:title>
+                Selecciona la ciudad y la especialidad
+              </template>
+              <v-row justify="center" align="center">
+                <v-col cols="5">
+                  <!-- <AccordionCities class="ma-1" /> -->
+                  <v-col>
+                    <v-autocomplete
+                      v-model="city"
+                      :items="this.ciudades"
+                      label="Ciudades"
+                      persistent-hint
+                    >
+                      <template v-slot:append-outer>
+                        <v-slide-x-reverse-transition mode="out-in">
+                        </v-slide-x-reverse-transition>
+                      </template>
+                    </v-autocomplete>
+                  </v-col>
+                </v-col>
+                <v-col cols="5">
+                  <!-- <SelectSpecialists class="ma-1"/> -->
+                  <v-col>
+                    <v-autocomplete
+                      v-model="especialidad"
+                      :items="this.especialidades"
+                      label="Especialidades"
+                      persistent-hint
+                    >
+                      <template v-slot:append-outer>
+                        <v-slide-x-reverse-transition mode="out-in">
+                        </v-slide-x-reverse-transition>
+                      </template>
+                    </v-autocomplete>
+                  </v-col>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn
+                    class="text-White ma-1 w-100 bg-PrincipalCyan"
+                    variant="text"
+                  >
+                    Buscar
+                  </v-btn>
+                </v-col>
+              </v-row>
             </v-row>
           </v-col>
         </v-row>
       </v-container>
     </div>
     <!-- Fin de Buscador  -->
+<MedicalSearchCard/>
     <v-card>
-      <v-container >
+      <v-container>
         <v-row>
           <v-col cols="6" align="center" justify="center">
-        <v-card class="formContainer pa-4 elevation-20">
+            <v-card class="formContainer pa-4 elevation-20">
+              <div class="card elevation-20">
+                <img
+                  src="ruta_de_la_imagen_del_medico.jpg"
+                  alt="Foto del médico"
+                />
+                <div class="card-content">
+                  <div class="card-title">Médico</div>
+                  <div class="card-info">
+                    <p>Nombre: Dr. Juan Pérez</p>
+                    <p>Edad: 35 años</p>
+                    <p>Especialidad: Pediatría</p>
+                    <p>Ciudad: Ciudad de México</p>
+                  </div>
+                  <button class="card-button">Agendar</button>
+                </div>
+              </div>
 
-            <div class="card elevation-20">
-    <img src="ruta_de_la_imagen_del_medico.jpg" alt="Foto del médico">
-    <div class="card-content">
-      <div class="card-title">Médico</div>
-      <div class="card-info">
-        <p>Nombre: Dr. Juan Pérez</p>
-        <p>Edad: 35 años</p>
-        <p>Especialidad: Pediatría</p>
-        <p>Ciudad: Ciudad de México</p>
-        </div>
-        <button class="card-button">Agendar</button>
-      </div>
-    </div>
-
-
-    <div class="card elevation-20">
-    <img src="ruta_de_la_imagen_del_medico.jpg" alt="Foto del médico">
-    <div class="card-content">
-      <div class="card-title">Médico</div>
-      <div class="card-info">
-        <p>Nombre: Dr. Juan Pérez</p>
-        <p>Edad: 35 años</p>
-        <p>Especialidad: Pediatría</p>
-        <p>Ciudad: Ciudad de México</p>
-        </div>
-        <button class="card-button">Agendar</button>
-      </div>
-    </div>
-
-  </v-card>
-  <v-col cols="6">
-    <div class="card elevation-20">
-    <img src="ruta_de_la_imagen_del_medico.jpg" alt="Foto del médico">
-    <div class="card-content">
-      <div class="card-title">Médico</div>
-      <div class="card-info">
-        <p>Nombre: Dr. Juan Pérez</p>
-        <p>Edad: 35 años</p>
-        <p>Especialidad: Pediatría</p>
-        <p>Ciudad: Ciudad de México</p>
-        </div>
-        <button class="card-button">Agendar</button>
-      </div>
-    </div>
-
-
-  </v-col>
-  </v-col>
-
-  </v-row>
-  </v-container>
-  </v-card>
-
-
-
+              <div class="card elevation-20">
+                <img
+                  src="ruta_de_la_imagen_del_medico.jpg"
+                  alt="Foto del médico"
+                />
+                <div class="card-content">
+                  <div class="card-title">Médico</div>
+                  <div class="card-info">
+                    <p>Nombre: Dr. Juan Pérez</p>
+                    <p>Edad: 35 años</p>
+                    <p>Especialidad: Pediatría</p>
+                    <p>Ciudad: Ciudad de México</p>
+                  </div>
+                  <button class="card-button">Agendar</button>
+                </div>
+              </div>
+            </v-card>
+            <v-col cols="6">
+              <div class="card elevation-20">
+                <img
+                  src="ruta_de_la_imagen_del_medico.jpg"
+                  alt="Foto del médico"
+                />
+                <div class="card-content">
+                  <div class="card-title">Médico</div>
+                  <div class="card-info">
+                    <p>Nombre: Dr. Juan Pérez</p>
+                    <p>Edad: 35 años</p>
+                    <p>Especialidad: Pediatría</p>
+                    <p>Ciudad: Ciudad de México</p>
+                  </div>
+                  <button class="card-button">Agendar</button>
+                </div>
+              </div>
+            </v-col>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </div>
 </template>
-<script>
-</script>
 
 <style>
 .card {
-      width: 400px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      padding: 20px;
-      display: flex;
-      align-items: flex-start;
-    }
-    .card img {
-      width: 100px;
-      height: 100px;
-      border-radius: 50%;
-      margin-right: 20px;
-    }
-    .card-content {
-      flex-grow: 1;
-    }
-    .card-title {
-      font-weight: bold;
-      font-size: 18px;
-      margin-bottom: 10px;
-    }
-    .card-info {
-      margin-bottom: 10px;
-    }
-    .card-button {
-      padding: 8px 16px;
-      background-color: #4caf50;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  width: 400px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  display: flex;
+  align-items: flex-start;
+}
+.card img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-right: 20px;
+}
+.card-content {
+  flex-grow: 1;
+}
+.card-title {
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+.card-info {
+  margin-bottom: 10px;
+}
+.card-button {
+  padding: 8px 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
 
 .container {
   /* background: url('../../assets/images/Cyanwallpaper.jpg') no-repeat  center;*/
-  background-image: linear-gradient(to right top, #1c565c, #26747d, #31949f, #3bb6c3, #46d8e8);
+  background-image: linear-gradient(
+    to right top,
+    #1c565c,
+    #26747d,
+    #31949f,
+    #3bb6c3,
+    #46d8e8
+  );
   height: 1000px;
   margin: 0;
 }
@@ -199,9 +191,9 @@
 .backTransparent {
   background-color: transparent;
 }
-.buscadorTittle{
+.buscadorTittle {
   text-transform: uppercase;
-  font-size:2rem;
+  font-size: 2rem;
   font-weight: bold;
 }
 
