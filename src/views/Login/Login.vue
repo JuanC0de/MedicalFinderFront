e3fcf1c25b9ddd7ef35d8fea477a296f3c710c91
Juan Andrés Bolaño <juabo008@gmail.com>
2023-06-24 12:12:14 -0500

    Cambios a los estilos del login

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,85 +1,121 @@
 <script>
 export default {
-  components: {
-  },
-};
+    data: () => ({
+      show1: false,
+      form: false,
+      email: null,
+      password: null,
+      loading: false,
+    }),
+
+    methods: {
+      onSubmit () {
+        if (!this.form) return
+
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      required (v) {
+        return !!v || 'Field is required'
+      },
+    },
+  }
 </script>
 
 <template>
   <div class="loginContainer">
-    <div class="overlay">
+    <div>
       <!-- LOGN IN FORM by Omar Dsoky -->
-      <form>
+      <v-form v-model="form" @submit.prevent="onSubmit" class="formContainer">
         <!--   con = Container  for items in the form-->
-        <div class="con">
+        <div class="formContainerInside">
           <!--     Start  header Content  -->
-          <header class="head-form">
-            <h2>Log In</h2>
+          <header class="headForm">
+            <h2>Iniciar Sesión</h2>
             <!--     A welcome message or an explanation of the login form -->
-            <p>Inicio de sesión para dashboard MedicalFinder</p>
+            <p>El sistema identificará que rol cumples</p>
           </header>
           <!--     End  header Content  -->
-          <br>
+          <br />
           <div class="field-set">
-
-            <!--   user name -->
-            <span class="input-item">
-              <i class="fa fa-user-circle"></i>
-            </span>
             <!--   user name Input-->
-            <input class="form-input" id="txt-input" type="text" placeholder="@UserName" required>
+            <v-text-field
+              v-model="email"
+              :readonly="loading"
+              :rules="[required]"
+              class="mb-2 formInput"
+              clearable
+              label="Email"
+              color="gray"
+              variant="solo"
+            ></v-text-field>
+            <br />
 
-            <br>
-
-            <!--   Password -->
-
-            <span class="input-item">
-              <i class="fa fa-key"></i>
-            </span>
             <!--   Password Input-->
-            <input class="form-input" type="password" placeholder="Password" id="pwd" name="password" required>
+            <v-text-field
+              v-model="password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :readonly="loading"
+              :rules="[required]"
+              :type="show1 ? 'text' : 'password'"
+              class="mb-2 formInput"
+              clearable
+              label="Contraseña"
+              color="gray"
+              variant="solo"
+              @click:append="show1 = !show1"
+            ></v-text-field>
+            <br />
 
-            <!--      Show/hide password  -->
-            <span>
-              <i class="fa fa-eye" aria-hidden="true" type="button" id="eye"></i>
-            </span>
+            <!--      Forgot Password button-->
+            <v-btn variant="plain" class="btnFrgPass">
+              ¿Olvidaste tu contraseña?
+            </v-btn>
 
-
-            <br>
-            <!--        buttons -->
             <!--      button LogIn -->
-            <button class="log-in"> Log In </button>
+            <v-btn
+              :disabled="!form"
+              :loading="loading"
+              block
+              color="#46d8e8"
+              size="large"
+              type="submit"
+              variant="elevated"
+              class="btnLogin"
+            >
+              Acceder
+            </v-btn>
           </div>
 
           <!--   other buttons -->
           <div class="other">
-            <!--      Forgot Password button-->
-            <button class="btn submits frgt-pass">Forgot Password</button>
-            <!--     Sign Up button -->
-            <button class="btn submits sign-up">Sign Up
-              <!--         Sign Up font icon -->
-              <i class="fa fa-user-plus" aria-hidden="true"></i>
-            </button>
-            <!--      End Other the Division -->
+            <v-row>
+              <v-col cols="12" sm="6">
+                <!--     Sign Up button -->
+                <v-btn class="btnRegister btnLogin">
+                  Registrate como Paciente
+                </v-btn>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-btn class="btnRegister btnLogin">
+                  Registrate como Médico
+                </v-btn>
+                <!--      End Other the Division -->
+              </v-col>
+            </v-row>
           </div>
 
           <!--   End Conrainer  -->
         </div>
 
         <!-- End Form -->
-      </form>
+      </v-form>
     </div>
   </div>
 </template>
 
 <style>
-/* Fonts Form Google Font ::- https://fonts.google.com/  -:: */
-@import url('https://fonts.googleapis.com/css?family=Abel|Abril+Fatface|Alegreya|Arima+Madurai|Dancing+Script|Dosis|Merriweather|Oleo+Script|Overlock|PT+Serif|Pacifico|Playball|Playfair+Display|Share|Unica+One|Vibur');
-
-/* End Fonts */
-
-/* End Global rules */
-
 /* Start body rules */
 .loginContainer {
   width: 100%;
@@ -87,39 +123,30 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(to top, #46D8E8 0%, #c6f2f7 100%);
+  background-image: linear-gradient(to top, #46d8e8 0%, #c6f2f7 100%);
   background-attachment: fixed;
   background-repeat: no-repeat;
   /*font-family: 'Vibur', cursive; the main font
   font-family: 'Abel', sans-serif;*/
-  opacity: .95;
+  opacity: 0.95;
   /* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
 }
-
-
-
-/* |||||||||||||||||||||||||||||||||||||||||||||*/
-/* //////////////////////////////////////////// */
-
-
-
-
 /* End body rules */
 
 /* Start form  attributes */
-form {
-  width: 450px;
+.formContainer {
+  width: 500px;
   min-height: 500px;
   height: auto;
   border-radius: 5px;
   margin: 2% auto;
   box-shadow: 0 15px 50px rgb(81, 81, 81);
   padding: 2%;
-  background-image: linear-gradient(-225deg, #42CCDB 50%, #b6f5fc 50%);
+  background-image: linear-gradient(-225deg, #42ccdb 50%, #b6f5fc 50%);
 }
 
 /* form Container */
-form .con {
+.formContainerInside {
   display: -webkit-flex;
   display: flex;
 
@@ -133,7 +160,7 @@ form .con {
 }
 
 /* the header form form */
-header {
+.headForm {
   margin: 2% auto 10% auto;
   text-align: center;
 }
@@ -151,62 +178,10 @@ header p {
   color: white;
 }
 
-
-
-/* //////////////////////////////////////////// */
-/* //////////////////////////////////////////// */
-
-
-.input-item {
-  background: #fff;
-  color: #333;
-  padding: 14.5px 0px 15px 9px;
-  border-radius: 5px 0px 0px 5px;
-}
-
-
-
-/* Show/hide password Font Icon */
-#eye {
-  background: #fff;
-  color: #333;
-
-  margin: 5.9px 0 0 0;
-  margin-left: -20px;
-  padding: 15px 9px 19px 0px;
-  border-radius: 0px 5px 5px 0px;
-
-  float: right;
-  position: relative;
-  right: 1%;
-  top: -.2%;
-  z-index: 5;
-
-  cursor: pointer;
-}
-
 /* inputs form  */
-input[class="form-input"] {
-  width: 240px;
+.formInput {
+  width: 350px;
   height: 50px;
-
-  margin-top: 2%;
-  padding: 15px;
-
-  font-size: 16px;
-  /* font-family: 'Abel', sans-serif; */
-  color: #5E6472;
-
-  outline: none;
-  border: none;
-
-  border-radius: 0px 5px 5px 0px;
-  transition: 0.2s linear;
-
-}
-
-input[id="txt-input"] {
-  width: 250px;
 }
 
 /* focus  */
@@ -220,64 +195,11 @@ input:focus {
 
 /* input[type="text"] {min-width: 250px;} */
 /* buttons  */
-button {
-  display: inline-block;
-  color: #252537;
-
-  width: 280px;
-  height: 50px;
-
-  padding: 0 20px;
-  background: #46D8E8;
-  border-radius: 5px;
-
-  outline: none;
-  border: none;
-
-  cursor: pointer;
-  text-align: center;
-  transition: all 0.2s linear;
-
+.btnLogin {
   margin: 7% auto;
-  letter-spacing: 0.05em;
 }
-
-/* Submits */
-.submits {
-  width: 48%;
-  display: inline-block;
-  float: left;
-  margin-left: 2%;
-}
-
-/*       Forgot Password button FAF3DD  */
-.frgt-pass {
-  background: transparent;
-}
-
 /*     Sign Up button  */
-.sign-up {
-  background: #46D8E8;
+.btnRegister {
+  background: #46d8e8;
 }
-
-
-/* buttons hover */
-button:hover {
-  transform: translatey(3px);
-  box-shadow: none;
-}
-
-/* buttons hover Animation */
-button:hover {
-  animation: ani9 0.4s ease-in-out infinite alternate;
-}
-
-@keyframes ani9 {
-  0% {
-    transform: translateY(3px);
-  }
-
-  100% {
-    transform: translateY(5px);
-  }
-}</style>
+</style>
