76740262bc17440fbe278c1730caffbf34a6b8c6
Vanessa Silva <Karelyssilva08122003@gmail.com>
2023-06-27 23:16:51 -0500

    formulario agendar cita conectado con la api

--- src/layouts/FormMedicalAppointments/FormMedicalAppointments.vue
+++ src/layouts/FormMedicalAppointments/FormMedicalAppointments.vue
@@ -1,31 +1,41 @@
 <script>
 import ServiceMedicalAppointments from "@/views/Find/MedicalAppointments/ServiceMedicalAppointments"
-import { useField } from "vee-validate";
+import swal from "sweetalert";
 import moment from 'moment';
 export default {
   data: () => ({
-    fecha: '',
-    fechaInvalida: false,
-    hora: "",
-    rules: [
-      value => !!value || 'Requerido',
-      value => (value && value.length >= 3) || 'Debe escribir un mensaje de minimo 3 caracteres.',
-    ],
+    form: false,
+    loading: false,
+    reason: null,
+    queryType: null,
+    date: null,
+    hour: "11:00:00",
+    IdEspecialista: "0",
+    IdMedico: "3",
+    stage: "Pendiente",
+    IdPaciente: "7"
   }),
-  setup() {
-    const fecha = useField("fecha");
-    const motivo = useField("motivo");
-    const hora = useField("hora");
-
-    return {
-      fecha,
-      motivo,
-      hora
-    };
-  },
   methods: {
-    validarFecha() {
-      const fechaIngresada = moment(this.fecha, 'YYYY-MM-DD');
+    onSubmit() {
+      if (!this.form) return;
+
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+
+    },
+    required(v) {
+      return !!v || 'Este campo es requerido'
+    },
+    reasonValid(value) {
+      if (value?.length >= 10) {
+        return true;
+      } else {
+        return 'El motivo ingresado necesita más de 10 caracteres.';
+      }
+    },
+    dateValid() {
+      const fechaIngresada = moment(this.date, 'YYYY-MM-DD');
       const fechaActual = moment();
 
       if (fechaIngresada.isValid() && fechaIngresada.isAfter(fechaActual, 'day')) {
@@ -36,23 +46,46 @@ export default {
     },
     async insertarCitaNueva() {
       console.log("Entre a la funcion");
-      console.log("fecha", this.fecha.value.value);
-      console.log("motivo", this.motivo.value.value);
-      console.log("hora", this.hora.value.value);
+      console.log("fecha", this.date);
+      console.log("motivo", this.reason);
+      console.log("hora", this.hour);
+      console.log("tipo de consulta", this.queryType);
+      console.log("id especialista", this.IdEspecialista);
+      console.log("id medico", this.IdMedico);
+      console.log("estado", this.stage);
+      console.log("id paciente", this.IdPaciente);
 
-      let response = await ServiceDoctor.insertarCita(
-        this.fecha.value.value,
-        this.motivo.value.value,
-        this.hora.value.value,
-        "presencial",
-        "primero",
-        3
+
+      let response = await ServiceMedicalAppointments.insertarCita(
+        this.date,
+        this.reason,
+        this.hour,
+        this.queryType,
+        this.IdEspecialista,
+        this.IdMedico,
+        this.stage,
+        this.IdPaciente
       );
       console.log("Esta es la respuesta deploy: Medico", response);
       if (response.status == 201) {
         console.log("YA AGREGAMOSSSS");
+        swal({
+          title: "Tu cita ha sido agendada correctamente",
+          text: "Gracias por utilizar Medical Finder",
+          button: "Aceptar",
+        }).then(() => {
+          this.$refs.formMedicalAppointments.reset(); // Reiniciar el formulario
+          //redireccion a inicio sesion
+          this.$router.push('/');
+        });
       } else {
-        console.log("Ocurrió un error", response);
+        swal({
+          title: "Ocurrió un error",
+          text: response,
+          button: "Aceptar",
+        }).then(() => {
+          console.log("Ocurrió un error", response);
+        });
       }
     },
   },
@@ -61,15 +94,19 @@ export default {
 <template>
   <v-container class="text-center title-lg">
     <h2>Agenda tu cita!</h2>
-    <v-form>
-      <v-select class="mt-5" label="Tipo de consulta" :items="['Presencial', 'Virtual']"></v-select>
-      <v-textarea class="mb-5" label="Motivo de la cita" :rules="rules" hide-details="auto"></v-textarea>
-      
-          <v-text-field type="date" v-model="fecha" @input="validarFecha"></v-text-field>
-          <p class="errorText" v-if="fechaInvalida">La fecha ingresada es inválida o anterior a la fecha actual.</p>
-       
+    <v-form ref="formMedicalAppointments" v-model="form" @submit.prevent="onSubmit">
+
+      <v-select v-model="queryType" :readonly="loading" :rules="[required]" clearable class="mt-5"
+        label="Tipo de consulta" :items="['Presencial', 'Virtual']"></v-select>
+
+      <v-textarea v-model="reason" :readonly="loading" :rules="[required, reasonValid]" clearable class="mb-5"
+        label="Motivo de la cita" hide-details="auto"></v-textarea>
+
+      <v-text-field type="date" v-model="date" :rules="[required, dateValid]"></v-text-field>
+      <p class="errorText" v-if="fechaInvalida">La fecha ingresada es inválida o anterior a la fecha actual.</p>
+
       <v-row>
-  
+
         <v-col cols="12" sm="3">
           <v-btn class="me-4 mt-3 horabtn" color="DarkCyan" type="submit"> 11:00 </v-btn>
         </v-col>
@@ -85,12 +122,13 @@ export default {
         <v-col cols="12" sm="3">
           <v-btn class="me-4 mt-3 horabtn" color="DarkCyan" type="submit"> 14:00 </v-btn>
         </v-col>
-        </v-row>
+      </v-row>
 
 
-        <v-btn class="me-4 mt-10 agendarbtn" color="SecondaryCyan" type="submit">
-          Agendar
-        </v-btn>
+      <v-btn :disabled="!form" :loading="loading" class="me-4 mt-10 agendarbtn" color="SecondaryCyan" type="submit"
+        @click="insertarCitaNueva()">
+        Agendar
+      </v-btn>
     </v-form>
   </v-container>
 </template>
--- src/views/Find/MedicalAppointments/ServiceMedicalAppointments.js
+++ src/views/Find/MedicalAppointments/ServiceMedicalAppointments.js
@@ -1,39 +1,41 @@
 import Global from "@/Global";
 
 class ServiceMedicalAppointments {
-    async insertarCita(fecha, motivo,hora, Tipodeconsulta, IdEspecialista, IdMedico) {
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        console.log("Entre al insertarCita");
-        const raw = JSON.stringify({
-          "fecha": fecha,
-          "motivo": motivo,
-          "hora": hora,
-          "Tipodeconsulta": Tipodeconsulta,
-          "IdEspecialista": IdEspecialista,
-          "IdMedico": IdMedico
-        });
-    
-        console.log("raw",raw)
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-          };
-        
-        let request = "agendar/apiagendar/Agendar/";
-        let url = Global.API_URL + request;
-    
-        try {
-          const response = await fetch(url,requestOptions);
-          console.log("Esta es la respuesta - Cita",response);
-          return response
-          
-        } catch (error) {
-          console.log('Error:', error);
-        }
-      }
+  async insertarCita(date, reason, hour, queryType, IdEspecialista, IdMedico, stage, IdPaciente) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    console.log("Entre al insertarCita");
+    const raw = JSON.stringify({
+      "Fecha": date,
+      "Motivo": reason,
+      "Hora": hour,
+      "Tipodeconsulta": queryType,
+      "IdEspecialista": IdEspecialista,
+      "IdMedico": IdMedico,
+      "Estados": stage,
+      "IdPaciente": IdPaciente
+    });
+
+    console.log("raw", raw)
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    let request = "agendar/apiagendar/Agendar/";
+    let url = Global.API_URL + request;
+
+    try {
+      const response = await fetch(url, requestOptions);
+      console.log("Esta es la respuesta - Cita", response);
+      return response
+
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
 }
 
 export default new ServiceMedicalAppointments();
